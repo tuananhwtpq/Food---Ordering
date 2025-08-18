0dbd2a7e18ed1d7d13a84f99ca0f80de49aea67f
tuananhwtpq <tuananhwtpq@gmail.com>
2025-08-18 12:40:37 +0700

    Update build gradle

---
+++ app/src/main/java/com/example/food_order/ui/main/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.food_order.ui.main
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.food_order.R
+import com.example.food_order.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
